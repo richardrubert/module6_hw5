9cc933cc80b40214723f52768d500710d785faf4
Andrew.Little <andrewlittle@mail.weber.edu>
2017-02-14 16:38:52 +0000

    Initial commit

---
+++ quarks_hw5_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  quarks_hw5_task1.c
+ *       	Usage:  ./quarks_hw5_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:38:13 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Andrew Little (), andrewlittle@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
