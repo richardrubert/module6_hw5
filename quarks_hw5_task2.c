fccf916fe6d5d08b921f61a622e9dc20eaf31fb4
richardrubert <richard.rubert@gmail.com>
2017-02-14 17:23:49 +0000

    initial commit

---
+++ quarks_hw5_task2.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  quarks_hw5_task2.c
+ *       	Usage:  ./quarks_hw5_task2.c
+ *    Description:  :x
+ *
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:38:37 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Richard D Rubert (), richardrubert@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+
+/* Function Prototypes */
+
+/* Main Program */
+
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
